d287cf90a5a31f8c60295ea420cbe08282eeb7d9
Alastair Lynn <arplynn@gmail.com>
2009-07-18 16:48:40 +0100

    Did away with the half-hearted rbtree implementation and used the one from libstdc++.
    Signed-off-by: Alastair Lynn <arplynn@gmail.com>

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 CC=clang
+CXX=llvm-g++
 ARCH=i386
 CFLAGS=-O0 -gfull -pipe -Wall -arch $(ARCH)
 #CFLAGS=-O4 -DNDEBUG -pipe -arch $(ARCH)
 LDFLAGS=-L. -arch $(ARCH)
+AR=ar
+#AR=llvm-ar
 
-test: test.o libyarns.dylib
-	$(CC) $(LDFLAGS) -lyarns -o $@ $<
+test: test.o libyarns.a
+	$(CC) $(LDFLAGS) -o $@ $^
 	
-libyarns.dylib: pages.o sched_multilevel.o sched_roundrobin.o yarn.o smp_scheduler.o sched_random.o alloc.o rbtree.o
-	$(LD) -lc -dylib -exported_symbols_list exports.txt $(LDFLAGS) -o $@ $^ /usr/lib/dylib1.o
+libyarns.a: pages.o sched_multilevel.o sched_roundrobin.o yarn.o smp_scheduler.o sched_random.o alloc.o rbtree.o
+	$(AR) rcs $@ $^
 
 test.o: test.c config.h yarn.h yarns.h
 	$(CC) $(CFLAGS) -c -o $@ $<
@@ -34,8 +37,8 @@ smp_scheduler.o: smp_scheduler.c smp_scheduler.h scheduler.h config.h lock.h deb
 alloc.o: alloc.c alloc.h pages.h debug.h
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-rbtree.o: rbtree.c rbtree.h alloc.h
-	$(CC) $(CFLAGS) -c -o $@ $<
+rbtree.o: rbtree.cpp rbtree.h alloc.h
+	$(CXX) $(CFLAGS) -c -o $@ $<
 
 clean:
-	rm -f *.o test libyarns.dylib
+	rm -f *.o test libyarns.a
---
+++ rbtree.cpp
@@ -0,0 +1,140 @@
+extern "C"
+{
+#include "alloc.h"
+#include "rbtree.h"
+}
+
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+template <class T> class yallocator;
+
+template <> class yallocator<void>
+{
+public:
+	typedef void* pointer;
+	typedef const void* const_pointer;
+	typedef void value_type;
+	template <class U> struct rebind { typedef yallocator<U> other; };
+};
+
+template <class T> class yallocator
+{
+public:
+	typedef size_t size_type;
+	typedef ptrdiff_t difference_type;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+	typedef T& reference;
+	typedef const T& const_reference;
+	typedef T value_type;
+	template <class U> struct rebind { typedef yallocator<U> other; };
+	
+	yallocator()
+	{
+	}
+	
+	yallocator(const yallocator& o)
+	{
+	}
+	
+	template <class U> yallocator(const yallocator<U>&)
+	{
+	}
+	
+	~yallocator ()
+	{
+	}
+	
+	pointer address(reference x) const { return &x; }
+	const_pointer address(const_reference x) const { return &x; }
+	
+	pointer allocate(size_type len, yallocator<void>::const_pointer hint = 0)
+	{
+		return (pointer)yalloc(len);
+	}
+	
+	void deallocate(pointer p, size_type n)
+	{
+		yfree(p);
+	}
+	
+	size_type max_size () const
+	{
+		return ~(size_type)0;
+	}
+	
+	void construct ( pointer p, const T& val ) {}
+	void destroy ( pointer p ) {}
+};
+
+typedef map<unsigned long, void*, less<unsigned long>, yallocator<std::pair<const unsigned long, void*> > > rtree;
+
+struct _rbtree
+{
+	rtree tree;
+	void* operator new ( size_t len )
+	{
+		return yalloc(len);
+	}
+	
+	void operator delete ( void* ptr )
+	{
+		yfree(ptr);
+	}
+};
+
+rbtree* rbtree_new ()
+{
+	rbtree* rt = new rbtree;
+	return rt;
+}
+
+void rbtree_free ( rbtree* tree )
+{
+	delete tree;
+}
+
+void rbtree_insert ( rbtree* tree, unsigned long key, void* value )
+{
+	tree->tree[key] = value;
+}
+
+void rbtree_remove ( rbtree* tree, unsigned long key )
+{
+	rtree::iterator iter = tree->tree.find(key);
+	if (iter != tree->tree.end())
+		tree->tree.erase(iter);
+}
+
+bool rbtree_search ( rbtree* tree, unsigned long key, void** value )
+{
+	rtree::iterator iter = tree->tree.find(key);
+	if (iter != tree->tree.end())
+	{
+		*value = iter->second;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+unsigned long rbtree_size ( rbtree* tree )
+{
+	return tree->tree.size();
+}
+
+unsigned long rbtree_min ( rbtree* tree )
+{
+	return tree->tree.begin()->first;
+}
+
+unsigned long rbtree_max ( rbtree* tree )
+{
+	return tree->tree.rbegin()->first;
+}
+
